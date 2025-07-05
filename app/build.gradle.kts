875fbef102322d5311d72b54e93c2fa3c5e5873a
beatrizmarioti-ieg <b.tmarioti3.0@gmail.com>
2025-07-05 16:04:47 -0300

    feat: adicionar layout da tela de cadastro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,4 +47,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation ("androidx.navigation:navigation-fragment:2.7.7")
+    implementation ("androidx.navigation:navigation-ui:2.7.7")
 }
---
+++ app/src/main/java/com/bea/nutria/ui/Cadastro/CadastroActivity.java
@@ -0,0 +1,20 @@
+package com.bea.nutria.ui.Cadastro;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bea.nutria.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CadastroActivity extends AppCompatActivity {
+
+
+}
+
