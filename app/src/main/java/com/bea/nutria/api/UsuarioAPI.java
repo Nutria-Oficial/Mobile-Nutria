a918b30c2c1abaf477696e9b19b05bcd8c5fb67d
beatrizmarioti <b.tmarioti3.0@gmail.com>
2025-10-04 22:00:15 -0300

    feat(cadastro): consumir API SQL e criar usuário no Firebase Auth

---
+++ app/src/main/java/com/bea/nutria/api/UsuarioAPI.java
@@ -0,0 +1,46 @@
+package com.bea.nutria.api;
+
+import com.bea.nutria.model.Usuario;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface UsuarioAPI {
+    //Busca o usuario pelo email
+    @GET("usuarios/{email}")
+    Call<Usuario> buscarUsuario(@Path("email") String email);
+
+    //Cadastra o usuario
+    @POST("usuarios")
+    Call<Usuario> cadastrarUsuario(@Body Usuario usuario);
+
+    //Atualiza o email do usuario
+    @PATCH("usuarios/{id}/email")
+    Call<Usuario> atualizarEmailUsuario(@Path("id") Integer id, @Body String email);
+
+    //Atualiza o telefone do usuario
+    @PATCH("usuarios/{id}/telefone")
+    Call<Usuario> atualizarTelefoneUsuario(@Path("id") Integer id, @Body String telefone);
+
+    //Atualiza a senha do usuario
+    @PATCH("usuarios/{id}/senha")
+    Call<Usuario> atualizarSenhaUsuario(@Path("id") Integer id, @Body String senha);
+
+    //Atualiza a foto do usuario
+    @PATCH("usuarios/{id}/foto")
+    Call<Usuario> atualizarFotoUsuario(@Path("id") Integer id, @Body String foto);
+
+    //Atualiza o nome do usuario
+    @PATCH("usuarios/{id}/nome")
+    Call<Usuario> atualizarNomeUsuario(@Path("id") Integer id, @Body String nome);
+
+    //Atualiza a empresa do usuario
+    @PATCH("usuarios/{id}/empresa")
+    Call<Usuario> atualizarEmpresaUsuario(@Path("id") Integer id, @Body String empresa);
+
+
+}
