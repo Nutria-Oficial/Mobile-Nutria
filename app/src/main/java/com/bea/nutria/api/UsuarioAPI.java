9031896208056906462e3a3f681a08b47a8e8676
beatrizmarioti <b.tmarioti3.0@gmail.com>
2025-10-15 08:32:35 -0300

    fix: testando erro

--- app/src/main/java/com/bea/nutria/api/UsuarioAPI.java
+++ app/src/main/java/com/bea/nutria/api/UsuarioAPI.java
@@ -9,6 +9,7 @@ import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
+//alteraçãoa teste
 public interface UsuarioAPI {
     //Busca o usuario pelo email
     @GET("usuarios/{email}")
